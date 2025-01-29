a3a223d2658f4651cf88dae038cf665d3f8cdcb0
trembon <trembon@hotmail.com>
2025-01-30 00:06:03 +0100

    debug logging

--- src/PeacekeeperPlayer.cpp
+++ src/PeacekeeperPlayer.cpp
@@ -24,33 +24,42 @@ public:
 
             QuestStatus aHeroesBurdenStatus = player->GetQuestStatus(AHeroesBurden_QuestID);
             if (aHeroesBurdenStatus == QUEST_STATUS_REWARDED) {
-                FixOraclesOrFrenzyheartReputation(player, TheOracles_FactionID);
-                FixOraclesOrFrenzyheartReputation(player, FrenzyheartTribe_FactionID);
+                ReputationMgr& repMgr = player->GetReputationMgr();
+
+                FixOraclesOrFrenzyheartReputation(player, repMgr, TheOracles_FactionID);
+                FixOraclesOrFrenzyheartReputation(player, repMgr, FrenzyheartTribe_FactionID);
             }
         }
     }
 
 private:
-    void FixOraclesOrFrenzyheartReputation(Player* player, uint32 factionId) {
-        ReputationMgr& repMgr = player->GetReputationMgr();
+    void FixOraclesOrFrenzyheartReputation(Player* player, ReputationMgr& repMgr, uint32 factionId) {
+        LOG_INFO("module", "Fixing rep :: init {}", factionId);
         bool hasChange = false;
 
         ReputationRank rep_rank = player->GetReputationRank(factionId);
+        LOG_INFO("module", "Fixing rep :: rank {}", rep_rank);
         if (rep_rank == REP_HATED || rep_rank == REP_HOSTILE) {
             const FactionEntry* entry = sFactionStore.LookupEntry(factionId);
+            LOG_INFO("module", "Fixing rep :: loaded faction");
 
             const int32 repToFriendly = repMgr.ReputationRankToStanding(REP_FRIENDLY);
             repMgr.SetOneFactionReputation(entry, repToFriendly + 5001.f, false, REP_HONORED);
+            LOG_INFO("module", "Fixing rep :: set reputation");
 
             hasChange = true;
         }
 
+        LOG_INFO("module", "Fixing rep :: checking war");
         if (repMgr.IsAtWar(factionId)) {
+            LOG_INFO("module", "Fixing rep :: is at war");
             repMgr.SetAtWar(factionId, false);
+            LOG_INFO("module", "Fixing rep :: no longer at war");
             hasChange = true;
         }
 
         if (hasChange) {
+            LOG_INFO("module", "Fixing rep :: sending state for {}", factionId);
             repMgr.SendState(repMgr.GetState(factionId));
         }
     }
